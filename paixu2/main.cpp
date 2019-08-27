8f6961eda5f3be138b97f4d53d3f6e2276244fc7
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-08-27 17:42:13 +0800

    Initial Commit

---
+++ paixu2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  paixu2
+//
+//  Created by s20181102929 on 2019/8/27.
+//  Copyright © 2019 s20181102929. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
